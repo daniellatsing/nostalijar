9061858b4f118ed25eb20bd40b1205681f0a2186
johnjuneho <jho12@uw.edu>
2024-02-27 13:13:17 -0800

    Initial build

---
+++ app/src/main/java/edu/uw/ischool/jho12/nostalijar/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package edu.uw.ischool.jho12.nostalijar.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
